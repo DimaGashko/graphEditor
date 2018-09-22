d785b1a54cb5329f2d25849764937654b9d8f42c
DimaGashko <dgashko@mail.ua>
2018-09-23 00:41:04 +0300

    baseJs

---
+++ app/scripts/graph-editor.ts
@@ -0,0 +1,11 @@
+let GraphEditor = (function () {
+
+   class GraphEditor { 
+      constructor() { 
+         console.log('App inited!');
+      }
+   }
+
+   return GraphEditor;
+
+}());
---
+++ app/scripts/main.ts
@@ -0,0 +1,5 @@
+(function () {
+
+   var graphEditor = new GraphEditor;
+   
+}());
