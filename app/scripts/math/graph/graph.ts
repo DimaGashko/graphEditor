fb451fe0efe95ae8953c4b0c4edcf6c4b1c914b0
DimaGashko <dgashko@mail.ua>
2018-09-24 20:20:52 +0300

    parseAdjacencyMatrix

--- app/scripts/math/graph/graph.ts
+++ app/scripts/math/graph/graph.ts
@@ -65,4 +65,46 @@ export default class Graph {
       return [[]];
    }
 
+   static parseAdjacencyMatrix(matrix: number[][]): Graph { 
+      let graph = new Graph();
+      let vertices: Vertex[] = [];
+
+      let len = matrix.length;
+
+      //Создаем вершины
+      //Добавляем их в граф сразу (что бы сохранить последовательность)
+      //Сохраняет их в массиве, что бы иметь быстрый доступ по индексу
+      for (let i = 0; i < len; i++) {
+         let vertex = new Vertex();
+         vertices.push(vertex);
+         graph.addVertex(vertex);
+      }
+
+      for (let i = 0; i < len; i++) {
+         for (let j = 0; j < len; j++) { 
+            //Нет ребра
+            if (matrix[i][j] === 0) continue;
+
+            //Ориентированное ребро
+            if (matrix[i][j] !== matrix[j][i]) { 
+               graph.addEdge(
+                  new Edge(vertices[i], vertices[j], 'uni', matrix[i][j])
+               );
+            
+            //Не ориентированное ребро
+            } else {
+               //не ориентированные ребра добавляем только в одной половине матрицы
+               if (i < j) continue; 
+
+               graph.addEdge(
+                  new Edge(vertices[i], vertices[j], 'bi', matrix[i][j])
+               );
+            }
+
+         }
+      }
+
+      return graph;
+   }
+
 }
