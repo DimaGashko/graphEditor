1862ced667e8750b120fc123204446d97a20a61d
DimaGashko <dgashko@mail.ua>
2018-09-25 08:41:55 +0300

    Круг для петли

--- app/scripts/project-manager/workspace/ws_render.ts
+++ app/scripts/project-manager/workspace/ws_render.ts
@@ -77,6 +77,10 @@ export default class WSRender extends Component {
    }
 
    private drowEdge(edge: Edge) { 
+      if (edge.v1 === edge.v2) { 
+         this.drowLoopEdge(edge);
+      }
+
       let ctx = this.ctx;
       ctx.save();
 
@@ -97,8 +101,6 @@ export default class WSRender extends Component {
       ctx.stroke();
 
       //Текст
-      let text = `${targE.name}${(edge.weight !== 1) ? ` (${edge.weight})` : ''}`;
-
       ctx.font = `${targE.style.fontVariant} 
          ${targE.style.fontSize * zoom}px ${targE.style.fontFamily}`;
       
@@ -109,7 +111,7 @@ export default class WSRender extends Component {
       let offsetX = 0; (xy1.x < xy2.x) ? 10 : -10;
       let offsetY = (xy1.y < xy2.y) ? 10 : -10;
 
-      ctx.fillText(text,
+      ctx.fillText(this.getEdgeText(edge),
          (xy1.x + xy2.x) / 2 + offsetX * zoom,
          (xy1.y + xy2.y) / 2 + offsetY * zoom
       );
@@ -141,6 +143,41 @@ export default class WSRender extends Component {
       }
 
       ctx.restore();
+   }
+
+   private drowLoopEdge(edge: Edge) { 
+      let ctx = this.ctx;
+      ctx.save();
+
+      let zoom = Math.max(this.data.zoom.x, this.data.zoom.y);
+      let targE: WSEdge = edge.targ; 
+      let targV: WSVertex = edge.v1.targ;
+
+      let xy = this.converter.toDisplay(targV.coords);
+      let r = targV.radius.x * zoom;
+
+      ctx.beginPath();
+      ctx.arc(xy.x - r, xy.y - r, r, 0, Math.PI * 2);
+      ctx.stroke();
+
+      //Текст
+      ctx.font = `${targE.style.fontVariant} 
+         ${targE.style.fontSize * zoom}px ${targE.style.fontFamily}`;
+      
+      ctx.fillStyle = targE.style.color;
+      ctx.textBaseline = "middle";
+      ctx.textAlign = "center";
+
+      ctx.fillText(this.getEdgeText(edge),
+         xy.x - r*2 + -5 * zoom,
+         xy.y - r*2 + -5 * zoom
+      );
+
+      ctx.restore();
+   }
+
+   private getEdgeText(edge: Edge): string { 
+      return `${edge.targ.name}${(edge.weight !== 1) ? ` (${edge.weight})` : ''}`
    }
 
    public updateMetrix(): void { 
