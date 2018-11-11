8fa281baa5da5dee6f33eb6b38948098d6cad2a1
DimaGashko <dgashko@mail.ua>
2018-11-12 00:02:12 +0200

    EpxComments

--- app/scripts/modelComponents/Expression.ts
+++ app/scripts/modelComponents/Expression.ts
@@ -1,26 +1,38 @@
 import Graph from "./graph/graph";
 
 /**
- * Математическое уравнение
+ * Математическое уравнение 
  * 
  * const exp = new Expression("2+3*6");
  * ext.strExt //"2+3*6"
  * exp.res //20
  * exp.tree //дерево выражения
+ * 
+ * Если нужен, например, только резултат:
+ * const res = new Expression("2+3*6").res;
  */
 export default class Expression { 
    private expTree: Graph<null, ExpNode> = null;
    private _res: number = null;
 
+   /**
+    * @param strExp Математическое выражение в виде строки 
+    */
    constructor(public readonly strExp: string) {     
       this.parse();
       this.calc();
    }
-
+   
+   /**
+    * @returns результат выражения
+    */
    public get res(): number { 
       return this._res;
    }
 
+   /**
+    * @returns представление графа в виде дерева
+    */
    public get tree() { 
       return this.expTree;
    }
