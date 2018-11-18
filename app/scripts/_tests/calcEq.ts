e585cca6cfe8bbe472fac6ac2b319d84e737bb82
DimaGashko <dgashko@mail.ua>
2018-11-18 11:22:11 +0200

    edit

--- app/scripts/_tests/calcEq.ts
+++ app/scripts/_tests/calcEq.ts
@@ -1,6 +1,12 @@
-import Expression from "../modelComponents/Expression";
+import Expression, { ExpNode } from "../modelComponents/Expression";
+import Workspace from "../workspace/workspace";
+import Graph from "../modelComponents/graph/graph";
+import WSEdge from "../workspace/ws_graph/ws_edge";
+import WSVertex from "../workspace/ws_graph/ws_vertex";
 
 const global = (<any>window);
+
+const workspace = new Workspace(document.querySelector('.workspace'));
 
 export default function demoCalcEq() {
    global.Expression = Expression;
@@ -12,5 +18,9 @@ export default function demoCalcEq() {
 }
 
 function setExp(exp: Expression) { 
-   
+   workspace.getData().wsGraph.graph = buildEpxGraph(exp.tree);
+}
+
+function buildEpxGraph(expTree: Graph<null, ExpNode>): Graph<WSEdge, WSVertex> { 
+   return new Graph<WSEdge, WSVertex>();
 }
